f48570cbf9230c56abccb2c5eca92441cff8384b
Nataliejaegeer <nataliejaeger93@gmail.com>
2025-10-10 14:58:23 +0200

    temp

---
+++ module-2/task_2-1/task_2-1.mjs
@@ -0,0 +1,105 @@
+"use strict";
+import { printOut, newLine } from "../../common/script/utils.mjs";
+
+
+/* Aktiver strict mode for å fange feil
+    "use strict";
+
+    // Part 2 og 3: Variabler og konstanter
+    let age = 18;                 // variabel (kan endres)
+    const name = "Natalie";       // konstant (kan ikke endres)
+    let price = 19.99;            // desimaltall
+    let greeting = "Hello!";      // string (tekst)
+    const birthYear = 2006;       // konstant med tall
+
+    */ 
+   // Et objekt med andre variabler
+    let student = {
+      firstName: "Natalie",
+      age: 18,
+      active: true
+    };
+
+    console.log("Age:", student.age);
+    //console.log("Name:", name);
+    //console.log("Price:", price);
+    //console.log("Greeting:", greeting);
+    //console.log("Birth Year:", birthYear);
+    //console.log("Student object:", student);
+
+    // Part 4: Implicit variable declaration (uten let/const/var)
+    // Dette gir en FEIL i strict mode
+    
+    
+    /*
+    try {
+      valuePI = 3.14;   // implicit declaration
+      console.log("valuePI:", valuePI);
+    } catch (error) {
+      console.error("Implicit declaration error:", error.message);
+    }
+
+    // Part 5: Forsøk på å endre en konstant
+    const myNumber = 100;
+    try {
+      myNumber = 200;   // prøver å endre konstant
+    } catch (error) {
+      console.error("Constant error:", error.message);
+    }
+*/
+
+
+printOut("--- Part 1 ----------------------------------------------------------------------------------------------");
+/* Put your code below here!*/
+printOut("Replace this with you answer!");
+let name_1 = "test";
+let name_2;
+printOut(name_1)
+// printOut(student.firstName);
+printOut(newLine);
+
+printOut("--- Part 2 ----------------------------------------------------------------------------------------------");
+/* Put your code below here!*/
+printOut("Replace this with you answer!");
+printOut(newLine);
+
+printOut("--- Part 3 ----------------------------------------------------------------------------------------------");
+/* Put your code below here!*/
+printOut("Replace this with you answer!");
+printOut(newLine);
+
+printOut("--- Part 4 ----------------------------------------------------------------------------------------------");
+/* Put your code below here!*/
+printOut("Replace this with you answer!");
+printOut(newLine);
+
+printOut("--- Part 5 ----------------------------------------------------------------------------------------------");
+/* Put your code below here!*/
+printOut("Replace this with you answer!");
+printOut(newLine);
+
+printOut("--- Part 6 ----------------------------------------------------------------------------------------------");
+/* Put your code below here!*/
+printOut("Replace this with you answer!");
+printOut(newLine);
+
+printOut("--- Part 7 ----------------------------------------------------------------------------------------------");
+/* Put your code below here!*/
+printOut("Replace this with you answer!");
+printOut(newLine);
+
+printOut("--- Part 8 ----------------------------------------------------------------------------------------------");
+/* Put your code below here!*/
+printOut("Replace this with you answer!");
+printOut(newLine);
+
+printOut("--- Part 9 ----------------------------------------------------------------------------------------------");
+/* Put your code below here!*/
+printOut("Replace this with you answer!");
+printOut(newLine);
+
+/* Task 10*/
+printOut("--- Part 10 ---------------------------------------------------------------------------------------------");
+/* Put your code below here!*/
+printOut("Replace this with you answer!");
+printOut(newLine);
