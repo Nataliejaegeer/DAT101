59fcf36d8fd049b10e1101ae5bfc27b864127d4a
Nataliejaegeer <nataliejaeger93@gmail.com>
2026-01-20 13:06:29 +0100

    endring

---
+++ module-5/Task_5-1_FlappyBird/Hero.js
@@ -0,0 +1,34 @@
+"use strict";
+import { TSprite } from "libSprite";
+import { EGameStatus } from "./FlappyBird.mjs";
+
+export class THero extends TSprite {
+    #gravity;
+    #speed;
+  constructor(aSpcvs, aSPI) {
+    super(aSpcvs, aSPI, 100, 100);
+    this.animationSpeed = 30;
+    this.#gravity = 9.81 / 100;
+    this.#speed = 0;
+  }
+
+  animate() {
+    if (this.y < 400-this.height) {
+      this.#speed+= this.#gravity;
+      this.y += this.#speed;
+      if(this.rotation < 90){
+        this.rotation = this.#speed * 15;
+    }
+  }
+  else {
+    EGameStatus.state = EGameStatus.gameOver;
+    this.animationSpeed = 0;
+  }
+}
+
+
+flap() {
+    this.#speed = -5;
+    this.rotation = 0;
+  }
+}
